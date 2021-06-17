dc054c1ab4c9f10295a5653f9ad6e497b6c96a28
Vasil <Vasil@ouc.com>
2021-06-17 22:29:07 +0800

    first commit

---
+++ src/main/java/com/xiao/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xiao.git;
+
+/**
+ * @Descriptioon:
+ * @author: xiao
+ * @create: 2021-06-17 22:24
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+    }
+}
